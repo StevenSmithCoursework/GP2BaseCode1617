8939838428111db24933c57a66a57485b8d9e172
StevenSmithCoursework <SSMITH214@caledonian.ac.uk>
2016-10-25 14:55:53 +0100

    End of Lab 5
    End of Lab 5

---
+++ GameApplication/include/GameObject.h
@@ -0,0 +1,55 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+#include "Common.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "Vertex.h"
+
+class GameObject
+{
+public:
+	GameObject();
+	~GameObject();
+
+	void onRender(mat4& view, mat4& projection);
+	void onUpdate();
+	void onInit();
+	void onDestroy();
+	
+	void loadTexture(const string& filename);
+	void loadShaders(const string& vsfilename, const string& fsfilename);
+	void copyVertexData(Vertex *pVerts, int numberOfVertices);
+
+private:
+	GLuint m_VBO;
+	GLuint m_VAO;
+	
+	//Shader Program
+	GLuint m_ShaderProgram;
+
+	//matrices
+	mat4 m_ViewMatrix;
+	mat4 m_ProjMatrix;
+	mat4 m_ModelMatrix;
+
+	GLuint m_Texture;
+	GLuint m_ClampSampler;
+
+	vec3 m_Position;
+	vec3 m_Rotation;
+	vec3 m_Scale;
+
+	mat4 m_TranslationMatrix;
+	mat4 m_RotationMatrix;
+	mat4 m_ScaleMatrix;
+
+	int m_NumberOfVertices;
+
+protected:
+
+};
+
+
+#endif
+
